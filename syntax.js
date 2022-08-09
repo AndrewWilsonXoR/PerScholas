7afbc327d2cc95366cd7aeedee81fc510aea1a90
AndrewWilsonXoR <110188732+AndrewWilsonXoR@users.noreply.github.com>
2022-08-09 19:14:55 -0400

    Add files via upload

--- syntax.js
+++ syntax.js
@@ -9,10 +9,6 @@
     let c = a * b
     let d = z / x
     let e = b - x
-    console.log(z)
-    console.log(c)
-    console.log(d)
-    console.log(e) 
     let aa = 10
     let bb = 20
     let cc = 30
@@ -24,9 +20,7 @@
     let ii = 90
     let jj = 100
     let kk = (aa + bb + cc + dd + ee) / 5 
-    console.log(kk)
     let ll = (ff + gg + hh + ii + jj) / 5
-    console.log(ll)
     let mm = (ll + kk) / 2
     let nn = 4
     let oo = 10
@@ -34,13 +28,20 @@
     let qq = -30
     let rr = 55
     let ss = (nn + oo + pp + qq + rr) / 5
-    console.log(ss)
     let tt = 68
     let uu = 7
     let vv = -22
     let ww = 9
     let xx = 100
     let yy = (tt + uu + vv + ww + xx) / 5
-    console.log(yy)
     let zz = (ss + yy) / 2
+    console.log(z)
+    console.log(c)
+    console.log(d)
+    console.log(e)
+    console.log(kk)
+    console.log(ll)
+    console.log(mm)
+    console.log(ss)
+    console.log(yy)
     console.log(zz)
