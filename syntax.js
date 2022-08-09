ae550d38180156c44f9bdd7a09278576eea385bc
AndrewWilsonXoR <110188732+AndrewWilsonXoR@users.noreply.github.com>
2022-08-09 18:59:41 -0400

    Add files via upload

---
+++ syntax.js
@@ -0,0 +1,46 @@
+
+    let myName = "Andrew"
+       console.log(myName)
+    let x = 5
+    let y = 12
+    let z = x + y
+    let a = 20
+    let b = 27
+    let c = a * b
+    let d = z / x
+    let e = b - x
+    console.log(z)
+    console.log(c)
+    console.log(d)
+    console.log(e) 
+    let aa = 10
+    let bb = 20
+    let cc = 30
+    let dd = 40
+    let ee = 50
+    let ff = 60
+    let gg = 70
+    let hh = 80
+    let ii = 90
+    let jj = 100
+    let kk = (aa + bb + cc + dd + ee) / 5 
+    console.log(kk)
+    let ll = (ff + gg + hh + ii + jj) / 5
+    console.log(ll)
+    let mm = (ll + kk) / 2
+    let nn = 4
+    let oo = 10
+    let pp = 22
+    let qq = -30
+    let rr = 55
+    let ss = (nn + oo + pp + qq + rr) / 5
+    console.log(ss)
+    let tt = 68
+    let uu = 7
+    let vv = -22
+    let ww = 9
+    let xx = 100
+    let yy = (tt + uu + vv + ww + xx) / 5
+    console.log(yy)
+    let zz = (ss + yy) / 2
+    console.log(zz)
