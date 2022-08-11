5c29496f8b6829cb7ec0eec252269f8d164d818e
AndrewWilsonXoR <110188732+AndrewWilsonXoR@users.noreply.github.com>
2022-08-11 09:07:54 -0400

    Add files via upload

---
+++ person.js
@@ -0,0 +1,81 @@
+//let alot = 200000
+//const person = {
+    ///fingers: [11, "yellow"] ,
+    //hairs: alot,
+   // personName: "Android 18",}
+//the main advantages to using Object Oriented Programming (OOP) are including but not limited to,
+//improved software-development productivity, software maintainability, faster development,
+//and lower cost of development through reused assets. pls sharks
+//console.log(person.fingers)
+//console.log(person.hairs)
+//console.log(person.personName)
+//console.log(Object.keys(person))
+//console.log(Object.values(person))
+
+    
+//for (let i = 0; i < 10; i++ ) {
+    //console.log(i)
+//}
+
+//for (let i = 10; i >= 1; i-- ) {
+    //console.log(i)
+//}
+
+// for (let i = 0; i <= 10; i++){
+//   if(i % 2 === 0 && i > 0){ 
+//    console.log(i)
+//    }
+//  }
+
+//for (let i = 6; i <= 60; i++){
+    //if(i % 3 === 0) {
+    //console.log(i)
+  //  }
+//}
+
+// let grade = 90
+//     if (grade >= 80 && grade <= 100){
+//     console.log("You did a good job")
+//     }
+//     else if (grade >= 70 && grade < 80){
+//         console.log("Keep trying")
+//     }
+//     else if (grade >= 60 && grade < 70){
+//         console.log("ehhhh")
+//     }
+//     else if (grade >= 55 && grade < 60){
+//         console.log("Not too good")
+//     }
+//     else{
+//         console.log("Bad Grade but you'll get em next time")
+//     }
+
+// for (let x = 10; x <= 100; x += 20) {
+//     console.log(x);
+//         if(x >= 90) {
+//         for (let x =20; x <=100; x += 20) console.log(x);
+//         }
+// }
+
+
+// my best crack at the homework.
+// for (let x = 1; x <= 100; x++){
+//     if(x % 3 === 0)
+//     console.log("fizz")
+//     else if(x % 5 === 0)
+//     console.log("buzz")
+//     else if(x % 15 === 0)
+//     console.log("fizzbuzz")
+//     console.log(x)
+// }
+
+// let movies = ["Parasite", "TheJoker", "1917"]
+//console.log(movies[2])
+
+// Google says this is the solution
+for (let i = 1; i < 101; i++) {
+    if(i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz")
+    else if (i % 5 == 0) console.log("Buzz")
+    else console.log(i);
+}
