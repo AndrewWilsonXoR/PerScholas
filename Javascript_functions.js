d7e8ddac803fe31a21c41e1eaab8e3cd321c1b54
AndrewWilsonXoR <110188732+AndrewWilsonXoR@users.noreply.github.com>
2022-08-11 21:14:21 -0400

    Add files via upload

---
+++ Javascript_functions.js
@@ -0,0 +1,93 @@
+// function 1
+// const maxOfTwoNumbers = (x,y) => {
+//     if (x >= y) {
+//       return x;
+//     } else {
+//       return y;
+//     }
+//   }
+  
+//   console.log(maxOfTwoNumbers(3, 9));
+
+
+  // function 2
+//   const maxOfThreeNumbers = (x, y, z) => {
+//     if(x > y && x > z) {
+//         return x
+//     }
+//     else if(y > z){
+//         return y
+//     }
+//     else {
+//         return z
+//     }
+//   }
+//    console.log(maxOfThreeNumbers(1 , 5 , 3))
+
+// function 3
+// const isCharAVowel = (word) => {
+// if (word === "a" || word=== "e" || word=== "i" || word=== "o" || word=== "u" || word === "y" ){
+
+//     return "True"
+// }
+//     else{
+//         return "False"
+//     }
+// }
+// console.log(isCharAVowel("o"))
+
+// function 4
+    // const array = [1 , 2 , 3 , 4]
+    // let sum = 0
+
+    // for (let i = 0; i < array.length; i++){
+    //     sum += array[i]
+    // }
+    // console.log(sum)
+
+// function 5
+    // const array = [2 , 4 , 6]
+    // let product = 1
+
+    // for (let i = 0; i < array.length; i++){
+    //     product *= array[i]
+    // }
+    // console.log(product)
+
+// function 6
+// let i = 0
+// while(i <= 20){
+//     let x=i * 3
+//     if(i % 2===0){
+//     }i++ 
+// } console.log(arguments.length)
+
+// function 7
+// const str = 'Hello!'
+// const strChunks = str.split("")
+// console.log(strChunks)
+// const strChunksReverse = strChunks.reverse()
+// const strReverse = strChunksReverse.join("")
+// console.log(strReverse)
+
+// function 8
+// var arr = [
+//     'first item',
+//     'second item is longer than the third one',
+//     'third longish item'
+//   ];
+  
+//   var lgth = 0;
+//   var longest;
+  
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i].length > lgth) {
+//       var lgth = arr[i].length;
+//       longest = arr[i];
+//     }
+//   }
+  
+//   console.log(longest);
+
+// function 9 
+    
